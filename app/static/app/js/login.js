d33ca0d909daa2df0efc3dbc8953d2585e305199
Juncheng Tang <tang184@purdue.edu>
2017-06-21 11:14:23 -0400

    loginform

---
+++ app/static/app/js/login.js
@@ -0,0 +1,15 @@
+$('#loginform').submit(function() {
+    // Get all the forms elements and their values in one step
+    //var values = $(this).serialize();
+
+	$.ajax({
+        url: 'some-url',
+        type: 'post',
+        dataType: 'json',
+        data: $('this').serialize(),
+        success: function(data) {
+        	console.log(data);
+        }
+    });
+    //console.log(values);    	
+});
