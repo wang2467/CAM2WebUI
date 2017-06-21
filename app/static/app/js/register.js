e9dd6aec53c32f4290f3e9055129a18baa559f0a
Juncheng Tang <tang184@purdue.edu>
2017-06-21 11:02:02 -0400

    return previous

--- app/static/app/js/register.js
+++ app/static/app/js/register.js
@@ -1,22 +1,17 @@
+
+
 $('#registerform').submit(function() {
     // Get all the forms elements and their values in one step
     //var values = $(this).serialize();
 
-    if ($('#password').val() == $('#cpassword').val()) {
-    	$.ajax({
-	        url: 'some-url',
-	        type: 'post',
-	        dataType: 'json',
-	        data: $('this').serialize(),
-	        success: function(data) {
-	        	console.log(data);
-	        }
-	    });
-	    //console.log(values);    	
-    } else {
-    	console.log("password not match")
-    }
-
-    
-
+	$.ajax({
+        url: 'some-url',
+        type: 'post',
+        dataType: 'json',
+        data: $('this').serialize(),
+        success: function(data) {
+        	console.log(data);
+        }
+    });
+    //console.log(values);    	
 });
